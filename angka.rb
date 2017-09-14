0b4e229ea6e5224358636248b50e17833f4d99df
Hamid Wahyudin <hamidwahyudin1987@gmail.com>
2017-09-14 21:55:49 +0700

    Add angka.rb

---
+++ angka.rb
@@ -0,0 +1,4 @@
+puts 2*3 #perkalian angka bulat => hasilnya bulat
+puts 1+1 #penjumlahan angka bulat => hasilnya bulat
+puts 4/3 #pembagian angka bulat => hasilnya bulat
+puts 4.0/3 #pembaian float dengan angka bulat => hasilnya float / pecahan
