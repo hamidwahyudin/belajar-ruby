bc7f1b241ec150f2936bf96e1176c4d54be177f9
Hamid Wahyudin <hamidwahyudin1987@gmail.com>
2017-09-14 22:08:24 +0700

    Add ifelse.rb

---
+++ ifelse.rb
@@ -0,0 +1,14 @@
+username = 'hamid'
+
+#if
+if username == 'hamid'
+	puts "Selamat datang #{username}"
+end
+
+#if else
+
+if username == 'hamid'
+	puts 'Selamat datang.'
+else
+	puts 'Username salah'
+end
